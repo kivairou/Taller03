8d6a85caf3ed09898027eb06b5e7f6700354da73
Kevin Zamora Riquelme <kevin.zamora@alumnos.ucn.cl>
2025-11-19 00:33:02 -0300

    Create TareaFactory

---
+++ Main/Main/TareaFactory.java
@@ -0,0 +1,8 @@
+package Main;
+
+public class TareaFactory {
+
+	public static Tarea crearTarea(String tipo, String id, String descripcion, Estado estado, String responsable, Complejidad complejidad, String fecha) {
+		
+	}
+}
