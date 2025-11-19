a56e739ef37a9a4353a4e392c1ef3a192e2f6c8a
DiegoSB20 <diego.seco@alumnos.ucn.cl>
2025-11-18 22:51:34 -0300

    Create Usuario.java

---
+++ Main/Usuario.java
@@ -0,0 +1,27 @@
+public class Usuario {
+	private String username;
+    private String password;
+    private String rol; // "Administrador" o "Colaborador"
+
+    public Usuario(String u, String p, String rol) {
+        this.username = u;
+        this.password = p;
+        this.rol = rol;
+    }
+
+    public String getUsername() { 
+    	return username; 
+    	}
+    
+    public String getPassword() { 
+    	return password; 
+    	}
+    
+    public String getRol() { 
+    	return rol; 
+    }
+    
+    public boolean isAdministrador() { 
+    	return rol.equalsIgnoreCase("Administrador"); 
+    	}
+}
