3f66488b45f7cb2d7910d615fa4ac4de18dbca1f
DiegoSB20 <diego.seco@alumnos.ucn.cl>
2025-11-21 15:40:20 -0300

    Create Colaborador.java

---
+++ Taller03/src/Colaborador.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Colaborador extends Usuario{
+
+	public Colaborador(String u, String p) {
+		super(u, p, "Colaborador");
+		
+	}
+
+	@Override
+	public void mostrarMenu(Sistema sistema) {
+		
+		
+	}
+
+}
