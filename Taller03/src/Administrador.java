27cc4c03272e43f05618757938834a3e0bbfe46a
DiegoSB20 <diego.seco@alumnos.ucn.cl>
2025-11-21 15:38:23 -0300

    Create Administrador.java

---
+++ Taller03/src/Administrador.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Administrador extends Usuario {
+
+	public Administrador(String u, String p) {
+		super(u, p, "Administrador");
+		
+	}
+
+	@Override
+	public void mostrarMenu(Sistema sistema) {
+		
+		
+	}
+
+
+
+}
