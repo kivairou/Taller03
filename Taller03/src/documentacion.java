2b4916043406650ebdc05a6cfd2a760e388e5c3a
DiegoSB20 <diego.seco@alumnos.ucn.cl>
2025-11-21 15:41:04 -0300

    Create documentacion.java

---
+++ Taller03/src/documentacion.java
@@ -0,0 +1,19 @@
+package src;
+
+import java.time.LocalDate;
+
+public class Documentacion extends Tarea{
+
+	public Documentacion(String id, String descripcion, String estado, String responsable, String complejidad,
+			LocalDate fechaCreacion) {
+		super(id, descripcion, estado, responsable, complejidad, fechaCreacion);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void accept(IVisitorTarea visitor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
