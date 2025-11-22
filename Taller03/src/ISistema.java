1b348c0f01baadaa9fc76334ff70f863c707d7d3
DiegoSB20 <diego.seco@alumnos.ucn.cl>
2025-11-21 22:39:49 -0300

    Update ISistema.java

--- Taller03/src/ISistema.java
+++ Taller03/src/ISistema.java
@@ -1,11 +1,13 @@
 package src;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public interface ISistema {
 	Usuario logeo(String user, String contraseña);
-	void menuAdmin(Administrador admin);
+
+	void menuAdmin(Administrador admin) throws IOException;
 	void menuColaborador(Colaborador colab);
 	void leerArchivos() throws FileNotFoundException;
-	
+
 }
