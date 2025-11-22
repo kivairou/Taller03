4ed1a57491129a7f7aae8a142abe6a3fda870406
DiegoSB20 <diego.seco@alumnos.ucn.cl>
2025-11-21 22:40:17 -0300

    Update Proyecto.java

--- Taller03/src/Proyecto.java
+++ Taller03/src/Proyecto.java
@@ -1,4 +1,5 @@
 package src;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,11 +8,13 @@ public class Proyecto {
 	private String nombre;
 	private String responsable;
 	private List<Tarea> tareas = new ArrayList<>();
+	private PrioridadStrategy estrategia;
 
 	public Proyecto(String id, String nombre, String responsable) {
 		this.id = id;
 		this.nombre = nombre;
 		this.responsable = responsable;
+		this.estrategia = null;
 	}
 
 	public String getId() {
@@ -34,6 +37,20 @@ public class Proyecto {
 		tareas.add(t);
 	}
 
+	public void setEstrategia(PrioridadStrategy estrategia) {
+		this.estrategia = estrategia;
+	}
+
+	public ArrayList<Tarea> getTareasOrdenadas() {
+		if (estrategia == null) {
+			return new ArrayList<>(tareas);
+			// lista sin ordenar
+		} else {
+			return estrategia.tareasOrdenada(new ArrayList<>(tareas));
+			// lista ordenada port estrategia
+		}
+	}
+
 	public void borrarTarea(String tid) {
 		for (int i = 0; i < tareas.size(); i++) {
 			if (tareas.get(i).getId().equals(tid)) {
@@ -43,3 +60,4 @@ public class Proyecto {
 		}
 	}
 }
+
